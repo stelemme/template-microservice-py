1785477da0e52204fec260eb56b8befd3c14cd30
stelemme <stef.lemmens@ugent.be>
2023-05-05 16:07:47 +0200

    first commit

---
+++ flaskr/operation.py
@@ -0,0 +1,40 @@
+from flask import (
+    Blueprint, render_template, request, make_response
+)
+
+bp = Blueprint('operation', __name__, url_prefix='/op')
+
+@bp.route('/', methods=['GET'])
+def description():
+    return render_template('operation.html')
+
+@bp.route('/some-operation', methods=('GET', 'POST'))
+def operation():
+    if request.method == 'GET':
+        response = make_response({
+            "message": "Send data to this endpoint with an HTTP POST request to activate an operation on the data.",
+            "accepts": "application/json",
+            "returns": "application/json",
+        })
+        response.headers['Content-Type'] = 'application/json'
+        response.headers['Content-Length'] = 176
+
+        return response
+
+    elif request.method == 'POST':
+        data = request.get_json()
+
+        # Here some operation happens on the data, then a response is send.
+        # In this template we just return a confirmation that the data is successfully received.
+
+        print(data)
+
+        response = make_response({ "message": "Data received successfully" })
+        response.headers['Content-Type'] = 'application/json'
+        response.headers['Content-Length'] = 46
+
+        return response
+    
+    
+
+
